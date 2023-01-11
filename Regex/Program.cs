b3cd3875a9b81511e2449d0bb20a1cc9dc37ebaf
shivrjpatil <shivrajp972@gmail.com>
2023-01-11 20:36:33 +0530

    Added program to validate the first name

---
+++ Regex/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegulerExpression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ValidFirstName.Validate();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Regex/ValidFirstName.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegulerExpression
+{
+    internal class ValidFirstName
+    {
+        public static void Validate()
+        {
+            string pattern = "^[A-Z]{1}[a-z]{3,}$";
+            string[] Arr = { "Shivraj", "shivraj","Virat","Rohit" };
+            foreach (string input in Arr)
+            {
+                if(Regex.IsMatch(input,pattern))
+                {
+                    Console.WriteLine("{0} is valid",input);
+                }
+                else
+                {
+                    Console.WriteLine("{0} not valid", input);
+                }
+            }
+        }
+
+
+    }
+}
