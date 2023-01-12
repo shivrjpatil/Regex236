7cb3763e7cd5ce7e5935de9cd7719ac7d73178af
shivrjpatil <shivrajp972@gmail.com>
2023-01-12 16:49:24 +0530

    Addded program to validate EmailId

--- Regex/Program.cs
+++ Regex/Program.cs
@@ -12,14 +12,24 @@ namespace RegulerExpression
         {
             Console.WriteLine("Reguler Expression");
             Console.WriteLine("Choose one option");
+            Console.WriteLine("1.To Validate FirstName\n2.To Validate lastName\n3.To Validate EmailId");
             int option=Convert.ToInt32(Console.ReadLine());
             switch(option)
             {
                 case 1:
-                    Validate.ValidateFirstName();
+                    Console.WriteLine("Enter the first name");
+                    string Firstname=Console.ReadLine();
+                    Validate.ValidateFirstName(Firstname);
                     break;
                 case 2:
-                    Validate.ValidateLastName();
+                    Console.WriteLine("Enter the last name");
+                    string lastname=Console.ReadLine(); 
+                    Validate.ValidateLastName(lastname);
+                    break;
+                case 3:
+                    Console.WriteLine("Eneter the email id");
+                    string Email = Console.ReadLine();
+                    Validate.ValidateEmailId(Email);
                     break;
 
             }
--- Regex/ValidFirstName.cs
+++ Regex/ValidFirstName.cs
@@ -9,41 +9,42 @@ namespace RegulerExpression
 {
     internal class Validate
     {
-        public static void ValidateFirstName()
+        public static void ValidateFirstName(string FirstName)
         {
             string pattern = "^[A-Z]{1}[a-z]{3,}$";
-            string[] Arr = { "Shivraj", "shivraj","Virat","Rohit","Jk" };
-            foreach (string input in Arr)
-            {
-                if(Regex.IsMatch(input,pattern))
+                if(Regex.IsMatch(FirstName,pattern))
                 {
-                    Console.WriteLine("{0} is valid",input);
+                    Console.WriteLine("{0} is valid",FirstName);
                 }
                 else
                 {
-                    Console.WriteLine("{0} not valid", input);
+                    Console.WriteLine("{0} not valid",FirstName);
                 }
-            }
         }
-        public static void ValidateLastName()
+        public static void ValidateLastName(string LastName)
         {
             string pattern = "^[A-Z]{1}[a-z]{3,}$";
-            string[] Arr = { "Patil","patil", "Kohli", "Sharma", "Jk" };
-            foreach (string input in Arr)
-            {
-                if (Regex.IsMatch(input, pattern))
+           
+                if (Regex.IsMatch(LastName, pattern))
                 {
-                    Console.WriteLine("{0} is valid", input);
+                    Console.WriteLine("{0} is valid", LastName);
                 }
                 else
                 {
-                    Console.WriteLine("{0} not valid", input);
-                }
-            }
+                    Console.WriteLine("{0} not valid", LastName);
+                }            
         }
-
-
-
-
+        public static void ValidateEmailId(string Email)
+        {
+            string pattern = "^[a-zA-Z0-9]+([.][A-Za-z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]+([.][a-zA-Z]+)?$";
+            if (Regex.IsMatch(Email, pattern))
+                {
+                    Console.WriteLine("{0} is valid", Email);
+                }
+                else
+                {
+                    Console.WriteLine("{0} not valid", Email);
+                }
+        }
     }
 }
